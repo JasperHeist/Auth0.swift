8740f5ec7abd917d7f17a5afcfa567e8324b1a84
Jasper Heist <jasper@imagineproducts.com>
2021-02-09 09:12:23 -0500

    removed proposed change to base webAuth

--- Auth0/BaseWebAuth.swift
+++ Auth0/BaseWebAuth.swift
@@ -214,12 +214,6 @@ class BaseWebAuth: WebAuthenticatable {
             return AuthenticationServicesSessionCallback(url: logoutURL,
                                                          schemeURL: redirectURL,
                                                          callback: callback)
-        }else if #available(macOS 10.11, *) {
-            #if canImport(AppKit)
-            return AuthenticationServicesLegacySessionCallback(url:logoutURL,
-                                                               schemeURL: redirectURL,
-                                                               callback:callback)
-            #endif
         }
         #endif
         callback(false)
